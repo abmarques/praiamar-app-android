08c4658a7146fe7dfef9b336e746453e4b716746
unknown <abmarques.si@gmail.com>
2021-05-13 22:34:10 -0300

    MVP folders created

--- app/src/main/java/br/com/praiamar/views/activities/MainActivity.kt
+++ app/src/main/java/br/com/praiamar/views/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.praiamar
+package br.com.praiamar.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.praiamar.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
