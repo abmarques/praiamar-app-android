eef4df046c4c5534c9f57b31e72c9a83350a9da9
unknown <abmarques.si@gmail.com>
2021-05-14 21:37:37 -0300

    HomeActivity creation

---
+++ app/src/main/java/br/com/praiamar/views/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package br.com.praiamar.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.praiamar.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
