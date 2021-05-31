789598b95c989bb2842ffbb588e4bcccfc3f1381
unknown <abmarques.si@gmail.com>
2021-05-31 00:47:22 -0300

    Stored all information about the coast and beaches

--- app/src/main/java/br/com/praiamar/views/activities/BeachDetailsActivity.kt
+++ app/src/main/java/br/com/praiamar/views/activities/BeachDetailsActivity.kt
@@ -1,7 +1,11 @@
 package br.com.praiamar.views.activities
 
+import android.app.SearchManager
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.praiamar.R
 import kotlinx.android.synthetic.main.activity_beach_details.*
 
@@ -15,5 +19,26 @@ class BeachDetailsActivity : AppCompatActivity() {
         beachDetailDescriptionTxtView.text = intent.getStringExtra("beachDescription")
         beachDetailsImageView.setImageResource(intent.getIntExtra("beachImage", 0))
 
+
+        hoteisButton.setOnClickListener{
+            val intent  = Intent(Intent.ACTION_WEB_SEARCH)
+            intent.putExtra(SearchManager.QUERY, "Hotéis em ${beachDetailNameTxtView.text}")
+            openIntent(intent)
+        }
+
+        restaurantesButton.setOnClickListener{
+            val intent  = Intent(Intent.ACTION_WEB_SEARCH)
+            intent.putExtra(SearchManager.QUERY, "Restaurantes em ${beachDetailNameTxtView.text}")
+            openIntent(intent)
+        }
+
+    }
+
+    private fun openIntent(intent: Intent){
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }else {
+            Toast.makeText(this,"Ocorreu um erro interno." , Toast.LENGTH_LONG).show()
+        }
     }
 }
